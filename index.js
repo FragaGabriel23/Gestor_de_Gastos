ddc6313625161ef92899cc3ffab2f9d48de9bc7d
FragaGabriel23 <gabrielfraga2308@gmail.com>
2023-04-11 03:27:07 -0300

    ADD Botão Dell

--- index.js
+++ index.js
@@ -135,6 +135,11 @@ var CriarCard = () => {
             <li class="${classLi} list-group-item">${gastos[i].Titulo}</li>
             <li class="${classLi} list-group-item">${gastos[i].Data}</li>
             <li class="${classLi} list-group-item">Descrição: ${gastos[i].Descricao}</li>
+            <li class="${classLi} list-group-item"><button id="btn" onclick="DellCard(${i})">
+             <svg viewBox="0 0 15 17.5" height="17.5" width="15" xmlns="http://www.w3.org/2000/svg" id="icon">
+              <path transform="translate(-2.5 -1.25)" d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z" id="Fill"></path>
+             </svg></button>
+            </li>
         </ul>
 
         </div>
@@ -142,6 +147,14 @@ var CriarCard = () => {
         document.getElementById('total').innerHTML = `<h1>TOTAL: R$${Number(Total).toFixed(2)}</h1>`
     }
 
+
+}
+
+var DellCard = (index) => {
+
+    gastos.splice(index, 1);
+    CriarCard();
+    SalvarLocal();
 
 }
 
